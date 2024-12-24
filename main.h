8e6c0d01870980e1bc5c92506981d67a8034d666
Hasan Tayfur <116800397+hsntyfr@users.noreply.github.com>
2024-12-24 15:11:49 +0300

    Revert "printler temizlendi"
    This reverts commit 9611b6b4e87c4cedbe7c7053a30c322e111ccfa3.

--- main.h
+++ main.h
@@ -1,16 +1,13 @@
 void girdiBolucu(char* girdi, char* argv[], int* argc);
-//void girdiAl();
+void komutlariYazdir(char**, int*);
+void girdiAl();
 void quit();
 void aktifArkaPlanVarMi();
 void quitAktifArkaPlanVarMi();
+void pipeKomutYurut(char* argv[], int argc); // Pipe işlemi yapan fonksiyon
 void komutYurutucu(char* argv[], int argc);  // Komutları çalıştırmak için
 void sirali(char* argv[], int argc);         // Komutları sıralamak ve işlemi kontrol etmek için
 bool kontrol(char* argv[], int argc, char* karakterDizisi); // Tehlikeli karakterleri kontrol etmek için
 void girisYonlendime(char* argv[], int argc); // Giriş yönlendirmesi kontrolü
 void cikisYonlendime(char* argv[], int argc); // Çıkış yönlendirmesi kontrolü
 void yonlendirme(char* argv[], int argc, char yon); // Yönlendirme işlemleri (giriş/çıkış)
-void komutlariYazdir(char* argv[], int* argc);
-void girdiAl(char girdi[]);
-bool kontrol(char* argv[], int argc, char* karakterDizisi);
-void tekliKomutYurut(char* argv[], int argc, bool arkaplan);
-void pipeKomutYurut(char* argv[], int argc);
